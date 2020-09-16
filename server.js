c652d795c7c109e26cb99729f99d684d30169543
Ryan <ryan@ryanwans.com>
2020-09-15 22:01:13 -0400

    bruh

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var app = require('express')();
+var btoa=require('btoa'),atob=require('atob');
 
 app.use(function(req, res, next) {
     res.setHeader('EGLA_AUTH_TOKEN', 'rebar_'+btoa(Date.now()));
@@ -8,5 +9,7 @@ app.use(function(req, res, next) {
 app.get('/', function( res, res) {
     res.sendFile('login.html', {root: __dirname});
 })
+app.get('/dependencyCSS.CSS', (req, res) => {res.sendFile('system.css', {root: __dirname})})
+app.get('/dependencyMAIN.JS', (req, res) => {res.sendFile('system.js', {root: __dirname})})
 
 app.listen(9000, () => {console.log('[ryanwans-api] nectar-build completed')});
